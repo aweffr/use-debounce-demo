b6a8e30aafc354f6c595ebe07869e85b8444e280
guying18 <guying828@foxmail.com>
2022-11-03 23:53:58 +0800

    feat: init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+
+
+const data = Array(250).fill(0).map((_, idx) => idx);
 
 function App() {
+
+  useEffect(()=>{
+    console.log("hello world!")
+  }, [])
+
+  const onScroll = (e) => {
+    console.log("onScroll called!", e);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onScroll={onScroll} style={{height: '95vh', overflowY: 'scroll'}}>
+      {
+        data.map(item => (
+          <div key={item}>id: {item}</div>
+        ))
+      }
     </div>
   );
 }
